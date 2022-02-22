10fa391c812015c940e78b3edc5de8c419db8132
yuff1006 <yuffieevans@gmail.com>
2022-02-21 19:05:21 -0600

    minor fix on js

--- scripts/index.js
+++ scripts/index.js
@@ -6,25 +6,27 @@ const formTitle = document.querySelector(".edit-form__person-title");
 const profileName = document.querySelector(".profile__name");
 const profileTitle = document.querySelector(".profile__title");
 const formField = document.querySelector(".edit-form__form");
-formName.value = profileName.textContent;
-formTitle.value = profileTitle.textContent;
 
-editIcon.addEventListener("click", ()=> {
+function handleOpenForm() {
+  formName.value = profileName.textContent;
+  formTitle.value = profileTitle.textContent;
   editForm.classList.add("edit-form_open");
-})
+}
 
-xButton.addEventListener("click", ()=> {
+function handleCloseForm() {
   editForm.classList.remove("edit-form_open");
-})
+}
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = formName.value;
   profileTitle.textContent = formTitle.value;
-  console.log(formName.value)
   editForm.classList.remove("edit-form_open");
 }
 
+
+editIcon.addEventListener("click", handleOpenForm)
+xButton.addEventListener("click", handleCloseForm)
 formField.addEventListener("submit", handleProfileFormSubmit)
 
 
