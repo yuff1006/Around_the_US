391bb6c77fef8905e6c4c6396d731e500ef51244
yuff1006 <yuffieevans@gmail.com>
2022-02-25 21:45:13 -0600

    feat: add dynamically generated cards using JS

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,33 @@ const formTitle = document.querySelector("#edit-form-title");
 const profileName = document.querySelector(".profile__name");
 const profileTitle = document.querySelector(".profile__title");
 const formField = document.querySelector(".edit-form__form");
+const cardsContainer = document.querySelector(".cards__container");
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
 
 function openPopup(form) {
   form.classList.add("edit-form_open");
@@ -50,3 +77,21 @@ editIcon.addEventListener("click", handleOpenForm);
 closeButton.addEventListener("click", handleCloseForm);
 closeButtonPicture.addEventListener("click", handleCloseForm);
 formField.addEventListener("submit", handleProfileFormSubmit);
+
+// code for generating cards dynamically
+function createCard(item) {
+  const cardTemplate = document.querySelector("#card").content;
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__img");
+  const cardTitle = cardElement.querySelector(".card__place");
+  cardImage.alt = item.name;
+  cardImage.src = item.link;
+  cardTitle.textContent = item.name;
+  return cardElement;
+}
+
+initialCards.map(function(item) {
+  const card = createCard(item);
+  cardsContainer.append(card);
+});
+
