dda7df9d282206f1ae1a4f35dfc3ba8c409dd297
yuff1006 <yuffieevans@gmail.com>
2022-02-27 22:59:49 -0600

    fix: variable naming change, tidy up const declaring, correct img popup from js to html

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,34 @@
+// profile icons
 const editIcon = document.querySelector(".profile__edit-icon");
 const addIcon = document.querySelector(".profile__add-icon");
-const editForm = document.querySelector("#edit-form");
-const pictureForm = document.querySelector(".edit-form_picture");
-const closeButton = document.querySelector("#edit-form-close-author");
-const closeButtonPicture = document.querySelector("#edit-form-close-picture")
-const formName = document.querySelector("#edit-form-name");
-const formTitle = document.querySelector("#edit-form-title");
-const formPlace = document.querySelector("#edit-form-place");
-const formUrl = document.querySelector("#edit-form-url");
+// author, add picture forms
+const editProfileForm = document.querySelector("#edit-form");
+const addPictureForm = document.querySelector(".edit-form_picture");
+// author, add pictures forms close buttons and popup picture close button
+const closeButtonAuthor = document.querySelector("#edit-form-close-author");
+const closeButtonPicture = document.querySelector("#edit-form-close-picture");
+const popupPictureClose = document.querySelector(".edit-form__close_picture");
+// profile form two fields
+const profileFormName = document.querySelector("#edit-form-name");
+const profileFormTitle = document.querySelector("#edit-form-title");
+// add picture form two fields
+const pictureFormPlace = document.querySelector("#edit-form-place");
+const pictureFormURL = document.querySelector("#edit-form-url");
+// profile display
 const profileName = document.querySelector(".profile__name");
 const profileTitle = document.querySelector(".profile__title");
+// form fields for the author form and the add picture form
 const formFieldAuthor = document.querySelector("#form-field-author");
 const formFieldPicture = document.querySelector("#form-field-picture");
+// cards and card title, card URL
 const cardsContainer = document.querySelector(".cards__container");
 const pictureTitle = document.querySelector("#edit-form-title");
 const pictureUrl = document.querySelector("#edit-form-url");
-const popupPictureClose = document.querySelector(".edit-form__close_picture");
+// picture popup container, caption, picture and close container
+const popupContainer = document.querySelector("#picture-popup");
+const popupCaption = document.querySelector(".edit-form__popup-caption");
+const popupPicture = document.querySelector(".edit-form__picture");
+popupCloseAndPicture = document.querySelector(".edit-form__close-and-picture");
 const initialCards = [
   {
     name: "Yosemite Valley",
@@ -42,7 +55,7 @@ const initialCards = [
     link: "https://code.s3.yandex.net/web-code/lago.jpg"
   }
 ];
-
+// functions to open and close the author and add picture forms
 function openPopup(form) {
   form.classList.add("edit-form_open");
   form.style.transition = "visibility 0s, opacity 0.5s linear";
@@ -53,92 +66,81 @@ function closePopup(form) {
   form.style.transition = "opacity ease-out 0.5s, visibility 0s ease-out 0.5s";
 
 }
-
-function handleOpenForm(evt) {
-  if (evt.target.id === "edit-icon") {
-    formName.value = profileName.textContent;
-    formTitle.value = profileTitle.textContent;
-    openPopup(editForm);
-  }
-  else if (evt.target.id === "add-icon") {
-    openPopup(pictureForm);
-  }
+function handleOpenProfileForm() {
+  profileFormName.value = profileName.textContent;
+  profileFormTitle.value = profileTitle.textContent;
+  openPopup(editProfileForm);
 }
-
-function handleCloseForm(evt) {
-  if (evt.target.id === "edit-form-close-picture") {
-    closePopup(pictureForm);
-  }
-  else if (evt.target.id === "edit-form-close-author") {
-    closePopup(editForm);
-  }
+function handleOpenAddPictureForm() {
+  openPopup(addPictureForm);
 }
-
+function handleCloseProfileForm() {
+  closePopup(editProfileForm);
+}
+function handleCloseAddPictureForm() {
+  closePopup(addPictureForm);
+}
+//functions to handle author and add picture form results
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  profileName.textContent = formName.value;
-  profileTitle.textContent = formTitle.value;
-  closePopup(editForm);
+  profileName.textContent = profileFormName.value;
+  profileTitle.textContent = profileFormTitle.value;
+  closePopup(editProfileForm);
 }
-
 function handlePictureFormSubmit(evt) {
   evt.preventDefault();
   const newCard = {};
-  newCard.name = formPlace.value;
-  newCard.link = formUrl.value;
+  newCard.name = pictureFormPlace.value;
+  newCard.link = pictureFormURL.value;
   cardsContainer.prepend(createCard(newCard));
-  closePopup(pictureForm);
+  closePopup(addPictureForm);
 }
-function picturePopupClose() {
-  const popupContainer = document.querySelector("#picture-popup");
+//functions to handle popup pictures close and open
+function openPicturePopup(evt) {
+  openPopup(popupContainer);
+  popupPicture.alt = evt.target.alt;
+  popupPicture.src = evt.target.src;
+  popupCaption.textContent = evt.target.alt;
+}
+function closePicturePopup() {
   closePopup(popupContainer);
 }
-
-addIcon.addEventListener("click", handleOpenForm);
-editIcon.addEventListener("click", handleOpenForm);
-closeButton.addEventListener("click", handleCloseForm);
-closeButtonPicture.addEventListener("click", handleCloseForm);
+//buttons for author, add picture, submit buttons for both and popup trigger's event listeners
+addIcon.addEventListener("click", handleOpenAddPictureForm);
+editIcon.addEventListener("click", handleOpenProfileForm);
+closeButtonAuthor.addEventListener("click", handleCloseProfileForm);
+closeButtonPicture.addEventListener("click", handleCloseAddPictureForm);
 formFieldAuthor.addEventListener("submit", handleProfileFormSubmit);
 formFieldPicture.addEventListener("submit", handlePictureFormSubmit);
-popupPictureClose.addEventListener("click", picturePopupClose);
+popupPictureClose.addEventListener("click", closePicturePopup);
 
 // code for generating cards dynamically
 function createCard(item) {
-  function heart(evt) {
-    evt.target.classList.toggle("card__heart_active");
-  }
-  function deleteCard(evt) {
-    evt.target.closest(".card").remove();
-  }
-  function picturePopup(evt) {
-    const popupContainer = document.querySelector("#picture-popup");
-    openPopup(popupContainer);
-    const popupPicture = document.createElement("img");
-    popupPicture.alt = evt.target.alt;
-    popupPicture.src = evt.target.src;
-    popupCaption.textContent = item.name;
-    popupPicture.classList.add("edit-form__picture");
-    popupCloseAndPicture = document.querySelector(".edit-form__close-and-picture");
-    if (popupCloseAndPicture.children.length > 2) {
-      popupCloseAndPicture.querySelector(".edit-form__picture").remove();
-    }
-    popupCloseAndPicture.prepend(popupPicture);
-  }
   const cardTemplate = document.querySelector("#card").content;
   const cardElement = cardTemplate.cloneNode(true);
   const cardImage = cardElement.querySelector(".card__img");
   const cardTitle = cardElement.querySelector(".card__place");
-  const popupCaption = document.querySelector(".edit-form__popup-caption");
   cardImage.alt = item.name;
   cardImage.src = item.link;
   cardTitle.textContent = item.name;
+  // interactivity for like button
+  function handleLike(evt) {
+    evt.target.classList.toggle("card__heart_active");
+  }
   const heartButton = cardElement.querySelector(".card__heart");
-  heartButton.addEventListener("click", heart);
+  heartButton.addEventListener("click", handleLike);
+  // function to delete cards using trash icon
+  function deleteCard(evt) {
+    evt.target.closest(".card").remove();
+  }
   const trashButton = cardElement.querySelector(".card__trash");
   trashButton.addEventListener("click", deleteCard);
-  cardImage.addEventListener("click", picturePopup);
+  // event listener for picture popup
+  cardImage.addEventListener("click", openPicturePopup);
+  // generate single card
   return cardElement;
 }
+//generate all six cards and append them to the card container
 initialCards.map(function(item) {
   const card = createCard(item);
   cardsContainer.append(card);
