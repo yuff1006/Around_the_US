50f1933061ff278dd06e98c73f486b3fd50098f5
yuff1006 <yuffieevans@gmail.com>
2022-03-07 10:36:41 -0600

    feat: add style to input on form validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -3,11 +3,64 @@ const settings = {
   inputSelector: ".edit-form__info",
   submitButtonSelector: ".edit-form__button",
   inactiveButtonClass: "edit-form__button_disabled",
+  // input line error style
   inputErrorClass: "edit-form__info_type_error",
+  // error message class
   errorClass: "edit-form__error_visible"
 }
 
-console.log(document.querySelector(`${settings.submitButtonSelector}`))
+function enableValidator() {
+  // getting all the forms in the whole page
+  const formList = Array.from(document.querySelectorAll(`${settings.formSelector}`));
+  // iterate over this array of all forms
+  formList.forEach((formEl)=> {
+    // prevent all forms from refreshing the page upon submit
+    formEl.addEventListener("submit", (evt)=> {
+      evt.preventDefault();
+    })
+    // for all forms, we need to set event listeners
+    setEventListeners(formEl);
+  })
+};
+// the forms are being iterated and within each of these forms, we are pulling and iterating through
+// inputs within each of these forms
+function setEventListeners(formEl) {
+  const inputList = Array.from(formEl.querySelectorAll(`${settings.inputSelector}`));
+  inputList.forEach((inputEl)=> {
+    inputEl.addEventListener("input", ()=> {
+      checkInputValidity(inputEl, formEl);
+    })
+  })
+};
+
+function checkInputValidity(inputEl, formEl) {
+  if (!inputEl.validity.valid) {
+    showInputError(inputEl, formEl);
+  }
+  else {
+    hideInputError(inputEl, formEl);
+
+  }
+}
+
+function showInputError(inputEl, formEl) {
+  // change teh input style upon error
+  inputEl.classList.add(`${settings.inputErrorClass}`);
+  // error message content
+  const errorMessage = inputEl.validationMessage;
+  // access the input id which is something like edit-form-description
+  const inputId = inputEl.id;
+  // the id of the span slot is the template literal
+  const spanSlot = formEl.querySelector(`#${inputId}-error`);
+  spanSlot.textContent = errorMessage;
+  spanSlot.classList.add(`${settings.errorClass}`);
+
+}
+
+function hideInputError(inputEl, formEl) {
+  inputEl.classList.remove(`${settings.inputErrorClass}`);
+}
 // enableValidation(
 
 // );
+enableValidator();
