45ef45b61bf1cc3f29a86d0a3cb49778f4dd3fdd
Yuffie Hu <yuffieevans@gmail.com>
2022-05-01 14:13:28 -0500

    feat: add API call for added cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,10 @@ const formFieldPicture = document.querySelector("#form-field-picture");
 // input fields for profile form popup
 const inputProfileName = document.querySelector("#popup-name");
 const inputProfileTitle = document.querySelector("#popup-title");
+// profile section on the page
+const profileName = document.querySelector(".profile__name");
+const profileTitle = document.querySelector(".profile__title");
+const profilePic = document.querySelector(".profile__pic");
 
 // add picture form functions
 function renderCard(inputValues) {
@@ -26,36 +30,77 @@ function renderCard(inputValues) {
   const cardEl = card.createCard();
   cardSection.addItem(cardEl);
 }
-const placePopup = new PopupWithForm(".popup_picture", (evt, inputValues) => {
-  handlePictureFormSubmit(evt, inputValues);
-});
 function handlePictureFormSubmit(inputValues) {
   renderCard(inputValues);
 }
+const placePopup = new PopupWithForm(".popup_picture", (inputValues) => {
+  fetch("https://around.nomoreparties.co/v1/group-12/cards", {
+    method: "POST",
+    headers: {
+      authorization: "1384428a-b01c-46ae-afda-f222b9d7dc7d",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: inputValues.name,
+      link: inputValues.link,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch(() => {
+      console.log("oops");
+    });
+  handlePictureFormSubmit(inputValues);
+});
+
 const imagePopup = new PopupWithImage("#picture-popup");
 function handleCardClick(image) {
   imagePopup.open(image);
 }
+
+// initialize card Section class variable to take api call result and interact with Section.js
 let cardSection;
+
 // profile form functions
 function fillProfileForm() {
   const result = userInfo.getUserInfo();
   inputProfileName.value = result.userName;
   inputProfileTitle.value = result.userJob;
 }
-const userInfo = new UserInfo({
-  nameSelector: ".profile__name",
-  jobSelector: ".profile__title",
-});
-const profilePopup = new PopupWithForm("#popup", (inputValues) => {
-  userInfo.setUserInfo(inputValues);
-});
 function handleOpenProfileForm() {
   formFieldAuthor.reset();
   fillProfileForm();
   addProfileFormValidator.resetValidation();
   profilePopup.open();
 }
+const userInfo = new UserInfo({
+  nameSelector: ".profile__name",
+  jobSelector: ".profile__title",
+});
+const profilePopup = new PopupWithForm("#popup", (inputValues) => {
+  fetch("https://around.nomoreparties.co/v1/group-12/users/me", {
+    method: "PATCH",
+    headers: {
+      authorization: "1384428a-b01c-46ae-afda-f222b9d7dc7d",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: inputValues.userName,
+      about: inputValues.userJob,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch(() => {
+      console.log("oops");
+    });
+
+  userInfo.setUserInfo(inputValues);
+});
 
 // validators
 const addProfileFormValidator = new FormValidator(settings, editProfileForm);
@@ -72,6 +117,7 @@ function handleOpenAddPictureForm() {
 addIcon.addEventListener("mouseup", handleOpenAddPictureForm);
 editIcon.addEventListener("mouseup", handleOpenProfileForm);
 
+// render intial cards
 fetch("https://around.nomoreparties.co/v1/group-12/cards", {
   headers: {
     authorization: "1384428a-b01c-46ae-afda-f222b9d7dc7d",
@@ -89,9 +135,7 @@ fetch("https://around.nomoreparties.co/v1/group-12/cards", {
     cardSection.renderItems();
   });
 
-const profileName = document.querySelector(".profile__name");
-const profileTitle = document.querySelector(".profile__title");
-const profilePic = document.querySelector(".profile__pic");
+//  render initial user profile
 fetch("https://around.nomoreparties.co/v1/group-12/users/me", {
   headers: {
     authorization: "1384428a-b01c-46ae-afda-f222b9d7dc7d",
